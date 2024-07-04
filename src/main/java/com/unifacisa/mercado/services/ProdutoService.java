d62c00028baf417c3a4fa1faf729a83c753ff336
Raffael Queiroga <raffaelqueirogaa@gmail.com>
2024-07-04 15:30:32 -0300

    implementação da classe de serviço da entidade Produto

--- src/main/java/com/unifacisa/mercado/services/ProdutoService.java
+++ src/main/java/com/unifacisa/mercado/services/ProdutoService.java
@@ -1,4 +1,62 @@
 package com.unifacisa.mercado.services;
 
+import com.unifacisa.mercado.entities.Produto;
+import com.unifacisa.mercado.repositories.ProdutoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class ProdutoService {
+
+    @Autowired
+    ProdutoRepository produtoRepository;
+
+
+    @Transactional
+    public void insert(Produto produto){
+        produtoRepository.save(produto);
+    }
+
+
+    @Transactional(readOnly = true)
+    public List<Produto> findAll(){
+        return produtoRepository.findAll();
+    }
+
+
+    @Transactional(readOnly = true)
+    public Produto findById(Long id){
+        Optional<Produto> produto = produtoRepository.findById(id);
+        return produto.orElse(null);
+    }
+
+
+    @Transactional
+    public Produto update(Long id , Produto produtoAtualizado){
+        Optional<Produto> optionalProduto = produtoRepository.findById(id);
+
+        if(optionalProduto.isPresent()){
+            Produto produto = optionalProduto.get();
+
+            produto.setNome(produtoAtualizado.getNome());
+            produto.setMarca(produtoAtualizado.getMarca());
+            produto.setValor(produtoAtualizado.getValor());
+
+            return produtoRepository.save(produto);
+        }else{
+            return null;
+        }
+
+    }
+
+
+    @Transactional
+    public void deleteById(Long id){
+        produtoRepository.deleteById(id);
+    }
+
 }
