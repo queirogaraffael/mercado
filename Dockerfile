df16ce33bd829fefef1f35880e599efe2c33e2b6
Raffael Queiroga <raffaelqueirogaa@gmail.com>
2024-09-03 09:59:34 -0300

    Dockerfile, docker-compose e outras configurações

---
+++ Dockerfile
@@ -0,0 +1,9 @@
+FROM eclipse-temurin:17-jdk
+
+WORKDIR /app
+
+COPY target/mercado-0.0.1-SNAPSHOT.jar /app/mercado.jar
+
+EXPOSE 8080
+
+CMD ["java", "-jar", "/app/mercado.jar"]
--- src/main/java/com/unifacisa/mercado/config/CacheCaffeineConfig.java
+++ src/main/java/com/unifacisa/mercado/config/CacheCaffeineConfig.java
@@ -2,28 +2,22 @@ package com.unifacisa.mercado.config;
 
 
 import com.github.benmanes.caffeine.cache.Caffeine;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cache.CacheManager;
-import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cache.caffeine.CaffeineCacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import java.util.concurrent.TimeUnit;
-
 @Configuration
 public class CacheCaffeineConfig {
+
+    @Value("${cache.caffeine.spec}")
+    private String caffeineSpec;
 
     @Bean
     public CacheManager cacheManager() {
         CaffeineCacheManager cacheManager = new CaffeineCacheManager();
-        cacheManager.setCaffeine(caffeineCacheBuilder());
+        cacheManager.setCaffeine(Caffeine.from(caffeineSpec));
         return cacheManager;
-    }
-
-    @Bean
-    public Caffeine<Object, Object> caffeineCacheBuilder() {
-        return Caffeine.newBuilder()
-                .expireAfterWrite(10, TimeUnit.MINUTES)
-                .maximumSize(100);
     }
 }
